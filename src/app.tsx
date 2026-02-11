d6258afd7d4c09dd8cb23c7fc5fd3537cc616912
kris1027 <krzys89@gmail.com>
2026-02-11 15:40:03 +0100

    chore: add Tailwind CSS v4 and shadcn/ui with base components

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Admin Dashboard</Button>
+    </div>
+  );
+};
+
+export default App;
