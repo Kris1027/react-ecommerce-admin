bd1c1b4318c6a0b0144832fe6c97c9a7d0bb8ef9
kris1027 <krzys89@gmail.com>
2026-02-14 14:11:08 +0100

    feat: add Tanstack Query with configured defaults and devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,16 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@/lib/query-client';
 
 const RootLayout = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
       <TanStackRouterDevtools position="bottom-right" />
-    </>
+    </QueryClientProvider>
   );
 };
 
