b5f1927a6e0c8f50770bd1e4f09f0facb39331bf
kris1027 <krzys89@gmail.com>
2026-03-15 13:06:50 +0100

    refactor: centralize money formatting in utils and use in revenue chart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const moneyFormatter = new Intl.NumberFormat('pl-PL', {
+  style: 'currency',
+  currency: 'PLN',
+});
+
+const moneyCompactFormatter = new Intl.NumberFormat('pl-PL', {
+  style: 'currency',
+  currency: 'PLN',
+  maximumFractionDigits: 0,
+});
+
+export const formatMoney = (amount: number): string =>
+  moneyFormatter.format(amount);
+
+export const formatMoneyCompact = (amount: number): string =>
+  moneyCompactFormatter.format(amount);
